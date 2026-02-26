4851ef2eaf7b71cfce8877d784a27fb37a6a4ed4
Jeremy-Burns0212 <208514124+Jeremy-Burns0212@users.noreply.github.com>
2026-02-26 09:52:44 -0800

    Added Product model

---
+++ JSEnhancedCRUD/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JSEnhancedCRUD.Models;
+
+/// <summary>
+/// A product for sale
+/// </summary>
+public class Product
+{
+	/// <summary>
+	/// A unique identifier for the product
+	/// </summary>
+	public int Id { get; set; }
+
+	/// <summary>
+	/// The customer facing name of the product
+	/// </summary>
+	[StringLength(30, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 30 characters.")]
+	[RegularExpression("^[a-zA-Z-0-9//s]*$", ErrorMessage = "Name must be alphanumeric characters only.")]
+	public required string Name { get; set; }
+
+	/// <summary>
+	/// The sales price in USD for the product
+	/// </summary>
+	public required decimal Price { get; set; }
+
+}
